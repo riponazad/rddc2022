5eea98f9e08b85bf8bb507d51e5a69fa6c5139cc
Md. Abulkalam Azad <39602054+riponazad@users.noreply.github.com>
2022-11-25 04:50:33 +0100

    Update prepare_train.py

--- yolov5/scripts/prepare_train.py
+++ yolov5/scripts/prepare_train.py
@@ -8,24 +8,24 @@ def parse_args():
 
     parser.add_argument("--images_dir", default="", help="Specify the path to the train images dataset folder.")
     parser.add_argument("--out_name", default="", help="Specify the file name to save the list.")
-    parser.add_argument("--annots_dir", default="", help="Specify the path to the annotaton (xmls) folder.")
+    #parser.add_argument("--annots_dir", default="", help="Specify the path to the annotaton (xmls) folder.")
 
     args = parser.parse_args()
     return args
 
 
-def create_list(images_dir, annots_dir, out_name):
+def create_list(images_dir, out_name):
     img_files = list(sorted(os.listdir(images_dir)))
-    annot_files = list(sorted(os.listdir(annots_dir)))
+    #annot_files = list(sorted(os.listdir(annots_dir)))
 
     n_samples = len(img_files)
-    n_train = int(n_samples*0.75)
+    #n_train = int(n_samples*0.75)
 
     with open(out_name+"_train.txt", "w") as f:
-        for i in range(n_train):
+        for i in range(n_samples):
             full_image_path = os.path.join(images_dir, img_files[i])
             f.write(str(full_image_path) + "\n")
-    with open(out_name+"_val.txt", "w") as f:
+    """ with open(out_name+"_val.txt", "w") as f:
         for i in range(n_train, n_samples):
             full_image_path = os.path.join(images_dir, img_files[i])
             f.write(str(full_image_path) + "\n")
@@ -37,12 +37,12 @@ def create_list(images_dir, annots_dir, out_name):
     with open(out_name+"annot_val.txt", "w") as f:
         for i in range(n_train, n_samples):
             full_annot_path = os.path.join(annots_dir, annot_files[i])
-            f.write(str(full_annot_path) + "\n")
+            f.write(str(full_annot_path) + "\n") """
 
 
 if __name__=='__main__':
     args = parse_args()
 
-    create_list(args.images_dir, args.annots_dir, args.out_name)
+    create_list(args.images_dir, args.out_name)
 
 
